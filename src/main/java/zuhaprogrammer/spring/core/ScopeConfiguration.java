8d1d5403828b07cbbc53eb07ccad4da7d6076174
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-09-10 20:13:01 +0700

    Custome scope bean

--- src/main/java/zuhaprogrammer/spring/core/ScopeConfiguration.java
+++ src/main/java/zuhaprogrammer/spring/core/ScopeConfiguration.java
@@ -1,10 +1,13 @@
 package zuhaprogrammer.spring.core;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import zuhaprogrammer.spring.core.data.Bar;
 import zuhaprogrammer.spring.core.data.Foo;
+import zuhaprogrammer.spring.core.scope.DoubletonScope;
 
 @Slf4j
 @Configuration
@@ -14,5 +17,19 @@ public class ScopeConfiguration {
     public Foo foo() {
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar() {
+        log.info("Create new bar");
+        return new Bar();
     }
 }
